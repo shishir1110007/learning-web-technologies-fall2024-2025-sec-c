fa2108d0fa6ad055b8f6b52aaac98bcf56a400b9
shishir1110007 <130244595+shishir1110007@users.noreply.github.com>
2024-12-15 16:41:59 +0600

    Add files via upload

---
+++ lab-Exam_set-B/Registration.php
@@ -0,0 +1,27 @@
+
+<?php
+?>
+    <fieldset>
+        <legend><b>REGISTRATION</b></legend>
+        Id </br>
+        <input type="text" name="" id=""> </br>
+        Password <br>
+        <input type="password" name="" id=""> <br>
+        Confirm Password <br>
+        <input type="password" name="" id=""> <br>
+        Name <br>
+        <input type="text" name="" id=""> </br>
+
+        User Type <hr><br>
+        <input type="radio" name="" id=""> User 
+        <input type="radio" name="" id=""> Admin  <hr> <br>
+        <input type="submit" name="" value="Sign Up">
+        <a href="">Sign In</a>
+
+
+
+
+
+    </fieldset>
+</form>
+Write to Web tech group
---
+++ lab-Exam_set-B/login.php
@@ -0,0 +1,17 @@
+<?php 
+?>
+
+<form method="POST">
+    <fieldset>
+        <legend><b>LOGIN</b></legend>
+        User Id </br>
+        <input type="text" name="id" id=""> </br>
+        Password <br>
+        <input type="password" name="password" id=""> <hr><br>
+
+        <input type="submit" name="" value="Login">
+        <a href="">Register</a>
+
+        </fieldset>
+</form>
+Write to Web tech group
